82da245233702df4f77c2865f4b2849f64ddc5da
Para Spidox <paradoxchimera@mailbox.org>
2025-07-17 17:30:44 +0100

    added comment

--- server/nextcloud.nix
+++ server/nextcloud.nix
@@ -2,12 +2,12 @@
 { config, pkgs, ... }:
 
 {
-environment.etc."nextcloud-admin-pass".text = "PWD";
-services.nextcloud = {
-	enable = true;
-	hostName = "nextcloud.test.spidox.ie";
-	config.adminpassFile = "/etc/nextcloud-admin-pass";
-	config.dbtype = "sqlite";
-	database.createLocally = true;
-};
+	environment.etc."nextcloud-admin-pass".text = "PWD"; # Just a placeholder. Not actually compromising myself :P
+	services.nextcloud = {
+		enable = true;
+		hostName = "nextcloud.test.spidox.ie";
+		config.adminpassFile = "/etc/nextcloud-admin-pass";
+		config.dbtype = "sqlite";
+		database.createLocally = true;
+	};
 }
