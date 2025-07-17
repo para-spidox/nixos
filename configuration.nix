52ff5b4a5018d0d35cbd96e094aef8eb45649d18
Para Spidox <paradoxchimera@mailbox.org>
2025-07-17 01:34:54 +0100

    added jellyfin service

--- configuration.nix
+++ configuration.nix
@@ -118,6 +118,11 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+  };
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
