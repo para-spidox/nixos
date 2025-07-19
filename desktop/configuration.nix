1345cfdbb6aa43c155065c237eb85cfe16598dca
Para Spidox <paradoxchimera@mailbox.org>
2025-07-19 23:51:26 +0100

    added applications, plymouth, v4l2loopback

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  boot.extraModulePackages = [
+    config.boot.kernelPackages.v4l2loopback.out
+  ];
+
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
@@ -40,9 +44,50 @@
   programs.firefox.enable = true;
 
   environment.systemPackages = with pkgs; [
+    blender
+    bottles
+    brave
+    davinci-resolve
     discord
+    krita
+    lutris
     nextcloud-client
+    obs-studio
+    signal-desktop
+    steam
+    telegram-desktop
     thunderbird
+    unityhub
+    vscode
   ];
-  
+ 
+  boot = {
+
+    plymouth = {
+      enable = true;
+#      theme = "rings";
+#      themePackages = with pkgs; [
+#        # By default we would install all themes
+#        (adi1090x-plymouth-themes.override {
+#          selected_themes = [ "rings" ];
+#        })
+#      ];
+    };
+
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader.timeout = 0;
+  };
+
 }
